0ba03115b40a88f4632b431c24158b2f57ca802f
Rob Howard <damncabbage@gmail.com>
2010-09-24 13:47:15 +1000

    Initial commit: dummy page, dummy placement edit controls, pulldown Scheduling tab with rough contents.

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Editing - This Ain't A Website</title>
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" href="css/screen.css">
+	<link rel="stylesheet" href="css/controls.css">
+</head>
+<body>
+
+	<div id="cms-toolbox">
+		<div class="contents">
+			<div class="scheduling">
+
+				<ul class="date-selection">
+					<li>+</li>
+					<li>January</li>
+					<li>2011</li>
+					<li>-</li>
+				</ul>
+
+				<div class="timeline-container zoom-month">
+					<div class="period-container">
+						<div class="period"><span class="title">1</span></div>
+						<div class="period"><span class="title">2</span></div>
+						<div class="period"><span class="title">3</span></div>
+						<div class="period"><span class="title">4</span></div>
+					</div>
+					<div class="bar"></div>
+					<div class="snapshot-container">
+						<a class="snapshot" href="#">2010-08-10 10:00am</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<ul class="tabs">
+			<li class="scheduling"><a href="#scheduling">Scheduling</a></li>
+		</ul>
+	</div>
+
+
+	<div class="container">
+		
+		<div class="heading">
+			<div class="cms-module-placement" id="cms-module-placement-heading">
+				<div class="edit-control">Heading</div>
+				<h1>This Ain't a Website</h1>
+				<h2>It's a Goddamn Mock-Up</h2>
+			</div>
+		</div>
+
+		<div class="body">
+			<div class="cms-module-placement" id="cms-module-placement-body">
+				<div class="edit-control">Body</div>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+				ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+				non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+				totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+				vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+				aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+				Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+				sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+				voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+				laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
+				qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
+				fugiat quo voluptas nulla pariatur?</p>
+
+				<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+				deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate
+				non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum
+				et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore,
+				cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat
+				facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
+				
+				<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
+				ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur
+				a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
+				doloribus asperiores repellat.</p>
+
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+				totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+				vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+				aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+				Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+				sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+				voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+				laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
+				qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
+				fugiat quo voluptas nulla pariatur?</p>
+			</div>
+		</div>
+
+		<div class="sidebar">
+			<div class="cms-module-placement" id="cms-module-placement-sidebar">
+				<div class="edit-control">Sidebar</div>
+
+				<h3>It's All Happening</h3>
+				<p>Consider the following links:</p>
+				<ul>
+					<li><a href="http://robhoward.id.au">RobHoward.id.au</a></li>
+					<li><a href="http://github.com/damncabbage">GitHub/damncabbage</a></li>
+					<li><a href="http://twitter/damncabbage">@damncabbage</a></li>
+					<li><a href="http://zombo.com">Zombo Com</a></li>
+				</ul>
+
+				<p>Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+				<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+				anim id est laborum.</p>
+			</div>
+		</div>
+
+	</div>
+
+	<script type="text/javascript" src="js/jquery-1.4.2.js"></script>
+	<script type="text/javascript" src="js/controls.js"></script>
+</body>
+</html>
---
+++ js/controls.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+	var cmsToolboxContentsHeight = $('#cms-toolbox > .contents').css('height');
+
+	$('#cms-toolbox > .tabs > .scheduling ').click(function(e){
+		var toolbox = $('#cms-toolbox');
+		toolbox.stop();
+
+		var bodyPaddingAdjustment = '-';
+		var targetTop = '-'+cmsToolboxContentsHeight;
+		if (toolbox.hasClass('hide')) {
+			targetTop = '0';
+			bodyPaddingAdjustment = '+';
+		}
+		toolbox.animate({ top: targetTop }, 400, 'swing', function(){ $(this).toggleClass('hide'); });
+		$('body').animate({ 'padding-top': bodyPaddingAdjustment+'='+cmsToolboxContentsHeight }, 400, 'swing');
+	});
+
+	/* Controls are initially turned off; degrade gracefully so that control is still usable with no JS. */
+//	$('#cms-toolbox').addClass('hide').css('top', '-'+cmsToolboxContentsHeight);
+
+
+
+
+	/* Dummy Edit Controls */
+	$('.cms-module-placement').mouseenter(function(e){
+		$(this).children('.edit-control').stop().show().animate({ opacity: 1 }, 'fast');
+	});
+	$('.cms-module-placement').mouseleave(function(e){
+		$(this).children('.edit-control').animate({ opacity: 0 }, 400, function(){ $(this).hide(); });
+	});
+
+	/* Controls are initially turned off; degrade gracefully so that control is still usable with no JS. */
+	$('.cms-module-placement .edit-control').addClass('hide'); 
+
+});
